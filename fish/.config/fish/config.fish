9260814637303bfbee84064426604a1e959a1403
Caio Raposo <caioraposo94@gmail.com>
2019-07-12 12:56:29 -0300

    Update for bspwm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,7 @@ function fish_greeting
 end
 
 export TERMINAL=alacritty
-export BROWSER=firefox
+export BROWSER=qutebrowser
 export EDITOR=nvim
 
 abbr vi "nvim"
@@ -17,14 +17,23 @@ abbr sp "sudo pacman"
 abbr i3conf "nvim ~/.config/i3/config"
 abbr polyconf "nvim ~/.config/polybar/config"
 abbr fishconf "nvim ~/.config/fish/config.fish"
-abbr viconf "nvim ~/.vimrc"
+abbr viconf "nvim ~/.config/nvim/init.vim"
 abbr alacriconf "nvim ~/.config/alacritty/alacritty.yml"
 abbr bspconf "nvim ~/.config/bspwm/bspwmrc"
-abbr sxhkdconf "nvim ~/.config/sxhkd/sxhkdrc"
+abbr keyconf "nvim ~/.config/sxhkd/sxhkdrc"
 
 export TRIMMOMATIC_ADAPTERS={$HOME}/miniconda3/share/trimmomatic/adapters/
 
 set PATH /usr/local/bin /usr/sbin $PATH
+
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
+
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!

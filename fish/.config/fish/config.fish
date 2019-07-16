dae6eb54c3959b5652c0166549b7024b92daa8f8
Caio Raposo <caioraposo94@gmail.com>
2019-07-15 21:22:50 -0300

    Update dots

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,19 +8,23 @@ end
 export TERMINAL=alacritty
 export BROWSER=qutebrowser
 export EDITOR=nvim
+export QT_QPA_PLATFORMTHEME="qt5ct"
 
 abbr vi "nvim"
 abbr vifzf "nvim (fzf)"
 abbr py "python3"
 abbr sp "sudo pacman"
+abbr sps "sudo pacman -S"
+abbr ys "yay -S"
+abbr yss "yay -Ss"
 
-abbr i3conf "nvim ~/.config/i3/config"
-abbr polyconf "nvim ~/.config/polybar/config"
-abbr fishconf "nvim ~/.config/fish/config.fish"
-abbr viconf "nvim ~/.config/nvim/init.vim"
-abbr alacriconf "nvim ~/.config/alacritty/alacritty.yml"
-abbr bspconf "nvim ~/.config/bspwm/bspwmrc"
-abbr keyconf "nvim ~/.config/sxhkd/sxhkdrc"
+abbr cfi3 		"nvim ~/.config/i3/config"
+abbr cfbar 		"nvim ~/.config/polybar/config"
+abbr cff 		"nvim ~/.config/fish/config.fish"
+abbr cfvi 		"nvim ~/.config/nvim/init.vim"
+abbr cfa 		"nvim ~/.config/alacritty/alacritty.yml"
+abbr cfbsp 		"nvim ~/.config/bspwm/bspwmrc"
+abbr cfk 		"nvim ~/.config/sxhkd/sxhkdrc"
 
 export TRIMMOMATIC_ADAPTERS={$HOME}/miniconda3/share/trimmomatic/adapters/
 

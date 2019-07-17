94c53105cc5bf83333b71eaeb8d13a9c807899f7
Caio Raposo <caioraposo94@gmail.com>
2019-07-17 19:22:34 -0300

    Add multi-monitor support and some fish abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,18 +5,20 @@ fish_vi_key_bindings
 function fish_greeting
 end
 
-export TERMINAL=alacritty
-export BROWSER=qutebrowser
-export EDITOR=nvim
-export QT_QPA_PLATFORMTHEME="qt5ct"
 
-abbr vi "nvim"
-abbr vifzf "nvim (fzf)"
+abbr vi	 "nvim"
+abbr vif "nvim (fzf)"
 abbr py "python3"
 abbr sp "sudo pacman"
 abbr sps "sudo pacman -S"
 abbr ys "yay -S"
 abbr yss "yay -Ss"
+abbr g "git"
+abbr gs "git status"
+abbr gb "git branch"
+abbr gc 'git commit -m "'
+abbr gch "git checkout"
+abbr ga "git add"
 
 abbr cfi3 		"nvim ~/.config/i3/config"
 abbr cfbar 		"nvim ~/.config/polybar/config"
@@ -26,7 +28,6 @@ abbr cfa 		"nvim ~/.config/alacritty/alacritty.yml"
 abbr cfbsp 		"nvim ~/.config/bspwm/bspwmrc"
 abbr cfk 		"nvim ~/.config/sxhkd/sxhkdrc"
 
-export TRIMMOMATIC_ADAPTERS={$HOME}/miniconda3/share/trimmomatic/adapters/
 
 set PATH /usr/local/bin /usr/sbin $PATH
 

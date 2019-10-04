a70f33b74a844a382abae25cedf986010beb9658
Caio Raposo <caioraposo@protonmail.com>
2019-10-04 07:26:54 +0000

    Add git prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,7 @@ fish_vi_key_bindings
 function fish_greeting
 end
 
-# Open file manager inside fish
+# Open file a manager inside fish
 function file_manager
     lf
 end
@@ -63,3 +63,44 @@ alias poetry_shell='. (dirname (poetry run which python))/activate.fish'
 eval /home/caio/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+
+# Show git branch in prompt
+# from: https://www.martinklepsch.org/posts/git-prompt-for-fish-shell.html
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
+function fish_prompt
+  set last_status $status
+
+  printf '%s' $USER '@' (hostname)
+  set_color $fish_color_cwd
+  printf ' %s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+  printf '\n> '
+
+  set_color normal
+end

dbbf380301c07d8df65fdf96c173dfa142f72e37
Caio Raposo <caioraposo@protonmail.com>
2020-07-01 16:44:36 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s $(pwd)/config/* $HOME/.config/
+ln -s $(pwd)/.xinitrc $HOME/.xinitrc 
+ln -s $(pwd)/.profile $HOME/.profile 
+ln -s $(pwd)/.xprofile $HOME/.xprofile 
+ln -s $(pwd)/.Xresources $HOME/.Xresources 
